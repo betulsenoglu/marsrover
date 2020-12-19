17faf53b852b807d86bff1f471cd7ae95a1dc516
Betül Şenoğlu <betul.senoglu@demirorenteknoloji.com>
2020-12-19 17:57:14 +0300

    nlog configuration is fired

--- MarsRover.App/Program.cs
+++ MarsRover.App/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
-
+﻿﻿using System;
+using NLog;
 namespace MarsRover
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
+            try
+            {
+                _logger.Log(LogLevel.Error, "DENEME");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
